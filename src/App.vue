b785ec2a5f472a0432847b6a873223e4ef076949
Higorami <114756431+Higorami@users.noreply.github.com>
2023-10-11 11:33:36 +0200

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <span id="nav-filler"></span>
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
@@ -9,16 +10,30 @@
 </template>
 
 <style>
+*{
+  margin: 0;
+  padding: 0;
+}
 #app {
+  padding: 0;
+  margin: 0;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
 }
-
+#nav-filler {
+  display: block;
+  height: 30px;
+}
 nav {
   padding: 30px;
+  background-color: rgb(112, 16, 156);
+  position: fixed;
+  top: 0px;
+  z-index: 100;
+  width: 100%;
 }
 
 nav a {
