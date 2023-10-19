3976a8a96e4f2ebf5a77bfedcb8c12bc8309cc0d
Higorami <114756431+Higorami@users.noreply.github.com>
2023-10-19 21:53:19 +0200

    Add files via upload

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,13 @@
   <div id="app">
     <span id="nav-filler"></span>
     <nav>
-        <router-link to="/" class="gauche">Accueuil</router-link>
-        <router-link to="/billets" class="gauche">Billets</router-link>
-        <router-link to="/services" class="gauche">Services</router-link>
+        <router-link to="/" class="gauche"><h2>Accueuil</h2></router-link>
+        <router-link to="/billets" class="gauche"><h2>Billets</h2></router-link>
+        <router-link to="/services" class="gauche"><h2>Services</h2></router-link>
+        <button id="box-langue" @click="changeLangue()">
+          <img src="../src/assets/images_drapeaux_langues/drapeau_france.png" alt="fr" v-show="francais">
+          <img src="../src/assets/images_drapeaux_langues/drapeau_angleterre.png" alt="en" v-show="!francais">
+        </button>
         <div class="authentification droite">
           <router-link to="/authentification"><img src="../src/assets/image_authentification.png" alt="authent"></router-link>
         </div>
@@ -12,6 +16,35 @@
     <router-view/>
   </div>
 </template>
+
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      francais: true,
+    }
+  },
+  methods: {
+    changeLangue(){
+      this.changeImageLangue();
+      this.changeTexteLangue();
+    },
+    changeImageLangue(){
+      this.francais = !this.francais;
+    },
+    changeTexteLangue(){
+      //TODO
+      if(this.francais){
+        //changer les textes en anglais
+      }else{
+        //changer les textes en francais
+      }
+    }
+  }
+}
+</script>
 
 <style>
 *{
@@ -29,7 +62,7 @@
 }
 #nav-filler {
   display: block;
-  height: 30px;
+  height: 50px;
 }
 nav {
   background-color: rgb(112, 16, 156);
@@ -44,26 +77,39 @@ nav a, nav div {
   font-weight: bold;
   color: #42f316;
   text-decoration: none;
-  height: 30px;
+  height: 50px;
   text-align: center;
 }
 
 .gauche{
-  border-right: #baee00 3px solid;
+  border-right: #baee00 5px solid;
   width: 20%;
-  padding-top: 15px;
+  padding-top: 25px;
+}
+
+#box-langue{
+  height: 50px;
+  background-color: transparent;
+  border: none;
+  margin-left: 5em;
+}
+
+#box-langue img{
+  height: 50px;
+  width: auto;
+  padding-top: 12.5px;
 }
 
 .droite{
   margin-left: auto;
   margin-right: 0;
-  width: 15%;
+  width: 65px;
   text-align: right;
 }
 
 .authentification img{
-  width: 45px;
-  height: 45px;
+  width: 75px;
+  height: 75px;
   filter: drop-shadow(0px 0px 5px #baee00);
 }
 
