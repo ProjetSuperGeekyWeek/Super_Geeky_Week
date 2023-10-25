78e63507b997cbc22a07f5b9ed4004cc4fbfa526
Higorami <114756431+Higorami@users.noreply.github.com>
2023-10-25 10:30:57 +0200

    Update AuthentificationView.vue

--- src/views/AuthentificationView.vue
+++ src/views/AuthentificationView.vue
@@ -1,5 +1,37 @@
 <template>
+    <!-- 
+        directe page authentification
+        (+ option changer mot de passe)
+        quand authentifié, redirection vers page d'accueil
+    -->
     <div class="authentification">
-        <h1>This is the authentification page</h1>
+        <img id="image-authentification" src="" alt="Image authentification">
+        <FormulaireAuthentification/>
     </div>
 </template>
+
+<script>
+    import FormulaireAuthentification from '@/components/FormulaireAuthentification.vue';
+
+    export default {
+        name: 'AuthentificationView',
+        components: {
+            FormulaireAuthentification
+        },
+        methods: {
+            authentification() {
+                this.email = FormulaireAuthentification.email;
+                this.password = FormulaireAuthentification.password;
+            }
+        },
+        computed: {
+            function : () => {
+                this.authentification();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
