70ce642b5671165cfbbeb472158d2e88282f5556
Higorami <fyleas.quaile@edu.univ-fcomte.fr>
2023-11-08 10:46:02 +0100

    ebauche style deconnects

--- src/components/AlreadyAuthentifier.vue
+++ src/components/AlreadyAuthentifier.vue
@@ -2,7 +2,7 @@
     <div id="authentifier">
         <h4 id="nom-authentifier">Bienvenue {{ nom }}</h4><br>
         <h5>Vous êtes déjà connecté</h5>
-        <div>
+        <div id="btn-box">
             <button id="change-mdp">Changer mot de passe</button>
             <button id="deconnect" @click="deconnect">Déconnexion</button>
         </div>
@@ -42,5 +42,27 @@ button {
     border-color: rgb(238, 103, 250);
     color: rgb(88, 3, 88);
 }
+
+#authentifier {
+    width: 50%;
+    height: 100%;
+    margin-left: 25%;
+    background-color: rgb(103, 250, 213);
+    border-radius: 5px;
+    border: 1px solid black;
+    padding: 10px;
+}
+
+@media screen and (orientation: portrait) {
+    button{
+        display: block;
+    }
+
+    #btn-box{
+        align-items: center;
+    }
+}
+
+
 
 </style>
