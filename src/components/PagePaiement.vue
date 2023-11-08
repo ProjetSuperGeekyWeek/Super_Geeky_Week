4ab3c515baf3771fd2ecc90a4f09c28139002ba6
Higorami <fyleas.quaile@edu.univ-fcomte.fr>
2023-11-08 13:57:57 +0100

    debut page prix

---
+++ src/components/PagePaiement.vue
@@ -0,0 +1,40 @@
+<template>
+    <p>Hello World</p>
+</template>
+
+<script>
+import { mapState } from 'vuex';
+
+export default {
+    name: 'PagePaiement',
+    data() {
+        return {
+            num_carte : '',
+            date_expiration : '',
+            code_securite : '',
+        }
+    },
+    props: {
+        listePrix : Array,
+        listePrestations : Array,
+        listeQuantite : Array,
+    },
+    methods: {
+    },
+    computed: {
+        ...mapState(['nom']),
+
+        prixTotal : function () {
+            let total = 0;
+            for(let i = 0; i < this.listePrix.length; i++) {
+                total += this.listePrix[i] * this.listeQuantite[i];
+            }
+            return total;
+        }
+    },
+}
+</script>
+
+<style scoped>
+
+</style>
