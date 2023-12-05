ba00170e2a2810029fbd0760c0ed35154e76aa73
Higorami <fyleas.quaile@edu.univ-fcomte.fr>
2023-12-05 08:05:03 +0100

    langue textes accueil presentation

--- front/src/assets/language/fr.js
+++ front/src/assets/language/fr.js
@@ -8,10 +8,11 @@ export default {
                 title: "Super Geeky Week",
 
                 accroche: "Bienvenue au plus grand événement geek Franc-Comtois !",
-                home_titre_1: "La Super Geeky Week c'est quoi ?",
+                home_titre_1: "Près de chez toi !",
                 home_text_1: "Plus besoin de te déplacer jusqu'à Paris pour assister à un événement geek majeur ! Avec la Super Geeky Week tu peux maintenant profiter de tous types d'activités geek près de chez toi. Localisé dans la nouvelle salle de Belfort, la Gigantomachie, tu pourras profiter de 3 jours de folie !",
-                home_titre_2: "Qu'est-ce qu'il y a à faire ?",
-                home_text_2: "Au programme toutes les activités dont tu peux rêver et plus encore ! Avec de merveilleux invités avec des séances d'autographes et des goodies par milliers pour être certain que tu gardes, même jusque chez toi, un souvenir inoubliable de ce Week-end fantastique !",            },
+                home_titre_2: "Une myriade d'activités !",
+                home_text_2: "Au programme toutes les activités dont tu peux rêver et plus encore ! Avec de merveilleux invités avec des séances d'autographes et des goodies par milliers pour être certain que tu gardes, même jusque chez toi, un souvenir inoubliable de ce Week-end fantastique !",            
+            },
         };
     },
 };
