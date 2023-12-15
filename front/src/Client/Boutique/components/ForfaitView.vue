e2e1e98651d243c39d74d80440eefaac003b903b
mleitao2-iut90 <manuel.leitao@edu.univ-fcomte.fr>
2023-12-15 11:46:16 +0100

    back boutique part 2

--- front/src/Client/Boutique/components/ForfaitView.vue
+++ front/src/Client/Boutique/components/ForfaitView.vue
@@ -4,16 +4,15 @@
     <p class="text_pres">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce turpis felis, vulputate vitae massa ac, efficitur accumsan ex. Phasellus lobortis hendrerit leo. Maecenas pellentesque feugiat tellus, eu pulvinar ex mattis varius. Sed nec nulla a massa maximus posuere. Curabitur diam dui, luctus efficitur mi vel, lacinia dignissim ex. Quisque at.</p>
     <div class="jour" v-for="jour in listCalendrier" :key="jour.id_calendrier">
       <p class="grand gras titre">Jour {{ jour.id_calendrier }} / Horaire de {{ jour.horaire_debut }} a {{ jour.horaire_fin }}</p>
-      <div class="forfait_jour" v-if="listCalendrier.id_calendrier = listItems.id_calendrier">
-        <div class="forfait_card" v-for="item in listCalendrier" :key="item.id_item">
+      <div class="forfait_jour">
+        <div class="forfait_card" v-for="item in filteredItems(jour.id_calendrier)" :key="item.id_calendrier">
           <p>{{ item.nom_item }}</p>
-          <p>Description {{ item.description_item }}</p>
+          <p>Description : {{ item.description_item }}</p>
           <p>Prix : {{ item.prix_item }}€</p>
           <p>Stock : {{ item.stock_item }}</p>
           <router-link to="/panier"><input type="button" value="Acheter" class="bouton grand_moins"></router-link>
         </div>
       </div>
-      <hr>
     </div>
   </div>
 </template>
@@ -42,17 +41,25 @@ export default {
       for (var i = 0; i < result.length; i++) {
         this.listCalendrier.push(result[i]);
       }
-      console.log(this.listCalendrier)
-    }
+      //console.log(this.listCalendrier)
+    },
   },
   created() {
     this.fillListeItems();
     this.fillListCalendrier();
-  }
+  },
+  computed: {
+    // Propriété calculée pour filtrer les items en fonction de l'id du calendrier
+    filteredItems() {
+      return (idCalendrier) => {
+        return this.listItems.filter(item => item.id_calendrier === idCalendrier);
+      };
+    },
+  },
 }
 </script>
 
-<style scoped>
+<style>
 *{
   margin: 0;
   padding: 0;
@@ -61,63 +68,48 @@ export default {
 
 .forfait{
   padding-top: 12%;
-  background-color: #9049bf;
-  color: #6cbad9;
+  background-color: var(--bg2);
 }
 
 .forfait_jour{
-  margin-left: 6%;
-  margin-right: 6%;
   display: flex;
-  padding-bottom: 2%;
-  justify-content: space-evenly;
+  flex-wrap: wrap;
+  justify-content: space-evenly ;
 }
 
 .forfait_card{
   display: flex;
   flex-direction: column;
-  justify-content: center;
-  padding: 2%;
-  text-align: center;
   align-items: center;
-  border-radius: 10px;
-  border: #490a73 3px solid;
-  background-color: #9249a6;
-  font-size: x-large;
-  transition: all .2s ease-in-out;
+  flex-wrap: wrap;
+  width: 20%;
+  border: 2px var(--title) solid;
+  border-radius: 20px;
+  padding: 1%;
+  background-color: var(--bg);
 }
+
 .forfait_card:hover{
   transform: scale(1.05);
 }
 
-.bouton{
-  border: 2px solid #490a73;
-  width: auto;
-  padding: 4%;
-  border-radius: 10px;
-  color: #6cbad9;
-  background-color: #9249a6;
-}
-
-.bouton:hover{
-  color: #9249a6;
-  background-color: #6cbad9;
-  cursor: pointer;
-}
-
 .forfait_card p{
-  margin-bottom: 10%;
+  padding-bottom: 2%;
+  color: var(--paragraph);
+  align-items:   center;
 }
 
 .titre{
   padding-top: 2%;
   padding-bottom: 1%;
+  color: var(--title);
 }
 
 .text_pres{
   margin-left: 10%;
   margin-right: 10%;
   font-size: xx-large;
+  color: var(--paragraph);
 }
 
 hr{
