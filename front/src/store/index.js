0d7873689b87f0db75146b7f193983a36921a858
mleitao2-iut90 <manuel.leitao@edu.univ-fcomte.fr>
2024-01-08 10:55:53 +0100

    crud add 2 trable

--- front/src/store/index.js
+++ front/src/store/index.js
@@ -506,6 +506,19 @@ export default new Vuex.Store({
         console.log(e);
       }
     },
+    async addNewRoleStore({commit}, nom_role){
+      let result = null
+      try{
+        result = await addNewRole(nom_role)
+        if (result.error === 0){
+          commit('SET_ALL_ROLE', result.data)
+        }else{
+          console.log(result.data)
+        }
+      }catch (err) {
+        console.log('anomalie dans addNewROleStore')
+      }
+    },
     /*async getAllStore({state,commit}, params){
       var responce;
         try{
