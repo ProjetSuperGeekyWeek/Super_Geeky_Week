f175dc7141253f4377976cf2e181b9fed65ea6e4
Joué Axel <113608406+Bresingre@users.noreply.github.com>
2024-01-09 08:39:03 +0100

    Add files via upload

---
+++ back/controllers/livredorController.js
@@ -0,0 +1,14 @@
+const livredorService = require('../services/livredorService');
+
+// get
+exports.getTemoignage = async (req, res) => {
+    livredorService.getTemoignage((err, data) => {
+        if (err) {
+            res.status(500).send({
+                message: err.message || "Some error occurred while retrieving temoignage."
+            });
+        } else {
+            res.send(data);
+        }
+    });
+}
