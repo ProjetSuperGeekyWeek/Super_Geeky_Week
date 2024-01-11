1b4f3d238ba302c9c5b4f1f3be4dc14b05e83829
Quentin <113607732+Senqen@users.noreply.github.com>
2024-01-11 02:01:58 +0100

    Update fr.js

--- front/src/assets/language/fr.js
+++ front/src/assets/language/fr.js
@@ -3,19 +3,16 @@ export default {
         return{
             fr: {
                 vid_not_supported:"Votre navigateur ne supporte pas la balise vidéo.",
-
                 nav_home: "Accueil",
                 nav_ticket: "Billets",
                 nav_espace_perso: "Ma Page",
                 nav_exposants: "Exposants",
                 title: "Super Geeky Week",
-
                 accroche: "Bienvenue au plus grand événement geek Franc-Comtois !",
                 home_titre_1: "Près de chez toi Franc-Comtois !",
                 home_text_1: "Plus besoin de te déplacer jusqu'à Paris pour assister à un événement geek majeur ! Avec la Super Geeky Week tu peux maintenant profiter de tous types d'activités geek près de chez toi. Localisé dans la nouvelle salle de Belfort, la Gigantomachie, tu pourras profiter de 3 jours de folie !",
                 home_titre_2: "Une myriade d'activités !",
                 home_text_2: "Au programme toutes les activités dont tu peux rêver et plus encore ! Avec de prestigieux invités avec des séances d'autographes et des goodies par milliers pour être certain que tu gardes, même jusque chez toi, un souvenir inoubliable de ce week-end fantastique !",
-
                 billet_title:"Billeterie",
                 billet_text:"Pour la Super Geeky Week, nous vous proposons trois types de forfaits pour rendre votre séjour encore plus agréable. Tout d'abord, le forfait standard vous permettra de profiter pleinement de tous les stands. Ensuite, nous vous offrons le forfait prioritaire qui vous garantit un accès plus rapide à nos services, vous permettant ainsi de maximiser votre temps et votre expérience. Enfin, pour assurer une accessibilité optimale, nous avons également le forfait dédié aux personnes en situation de handicap.",
                 jour:"Jour ",
@@ -26,55 +23,43 @@ export default {
                 prix2:"Prix",
                 prix3:"Prix :",
                 stock:"Stock : ",
-
                 prestation:"Prestations",
                 quantite:"Quantité",
                 soustotaux:"Sous-total",
-
                 rentrezinfo:"Renseignez vos informations",
                 prenom:"Prénom : ",
                 nom:"Nom : ",
                 email:"Adresse email : ",
                 validerinfo:"Valider les informations",
                 enteremail:"Entrer une adresse email valide",
-
                 information:"Information : ",
                 produit:"Produit",
-
                 panier:"Panier",
                 returnbout:"Retour boutique",
-
                 titre:"Titre :",
                 addpanier:"Ajouter au panier",
                 modifier:"Modifier",
                 supprimer:"Supprimer",
                 valider:"Valider",
                 annuler:"Annuler",
-
                 livror:"Livre d'or",
                 aucuntemoin:"Aucun témoignage disponible",
-
                 retour:"Retour",
                 desinscrire:"Désinscrire",
                 horaires:"Horaires",
                 inscription:"Inscription",
-
                 zetebieninscrit:"Vous êtes bien inscrit ",
                 seancede:" à la séance de ",
-
                 filre:"Filtre",
                 nopresta:"Aucun prestataire correspondant.",
-
                 exposants:"Exposants",
                 envoyer:"Envoyer",
                 aucunexpos:"Aucun exposant disponible.",
                 contactpresta:"Contact Prestataire",
                 voirprof:"Voir le profil",
-
                 nourriture:"Nourriture",
                 activit:"Vos activités :",
                 welcespace:"Bienvenue dans votre espace : ",
-
                 servicepage:"Page de service",
                 bienvenue:"Bienvenue ",
                 changemdp:"Changer le mot de passe",
@@ -83,7 +68,6 @@ export default {
                 mdp:"Mot de passe",
                 soumettre:"Soumettre",
                 acheter:"Acheter",
-
                 affinscrit:"Afficher inscrits",
                 cachinscrit:"Cacher inscrits",
                 addhoraire:"Ajouter un horaire",
@@ -102,6 +86,12 @@ export default {
                 personnetag:"Personne associée à un tag",
                 event:"Événement",
                 creneau:"Créneau",
+                afflivror:"Afficher le Livre d'or",
+                cachlivror:"Cacher le Livre d'or",
+                temoignage:"Témoignage",
+                addtemoin:"Ajouter un témoignage",
+                numcarte:"Numéro de carte",
+                datecarte:"Date d'expiration",
             },
         };
     },
