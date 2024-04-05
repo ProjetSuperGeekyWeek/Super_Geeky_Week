47bd3532dc7c0efeb2f35531d674f163b5952921
quaile fyleas <fyleas.quaile@edu.univ-fcomte.fr>
2024-04-05 09:33:20 +0200

    repare

--- back/routes/acheterRouter.js
+++ back/routes/acheterRouter.js
@@ -4,20 +4,6 @@ const acheterController = require("../controllers/crud/acheterController")
 
 //GET
 router.get('/getAllAcheter', acheterController.getAllAcheter);
-router.get('/getAllAcheterColumn', acheterController.getAllAcheterColumn);
-
-//POST
-router.post('/addAcheter', acheterController.addNewAcheter);
-
-//DELETE
-router.delete('/deleteAcheter', acheterController.deleteAcheterById);
-
-//PATCHconst express = require('express');
-const router = express.Router();
-const acheterController = require("../controllers/crud/acheterController")
-
-//GET
-router.get('/getAllAcheter', acheterController.getAllAcheter);
 router.get('/getAllAcheterWithItemNames', acheterController.getAllAcheterWithItemNames);
 router.get('/getAllAcheterColumn', acheterController.getAllAcheterColumn);
 
